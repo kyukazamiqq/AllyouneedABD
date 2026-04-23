55eed08ff3d9086c94bcb84e6beead251619ec71
kyukazamiqq <kyukazami@gmail.com>
2026-04-23 09:14:09 +0700

    first commit

--- start_demo.sh
+++ start_demo.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-DOCKER_COMPOSE_FILE=docker-compose-spark3.0.2.yml
-
+DOCKER_COMPOSE_FILE="docker-compose-spark3.0.2.yml"
 SCRIPT_PATH=$(cd `dirname $0`; pwd)
-docker-compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} down
-docker-compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} pull
+
+# Gunakan tanda kutip double (") untuk membungkus path file
+COMPOSE_CMD="docker compose -f \"${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE}\""
+
+echo "Menghentikan kontainer lama..."
+eval $COMPOSE_CMD down
+
+echo "Menarik image terbaru..."
+eval $COMPOSE_CMD pull
 sleep 5
 
-docker-compose -f ${SCRIPT_PATH}/${DOCKER_COMPOSE_FILE} up -d
+echo "Menjalankan kontainer..."
+eval $COMPOSE_CMD up -d
 sleep 15
 
+# Menjalankan perintah di dalam kontainer
+echo "Konfigurasi HDFS dan Spark..."
 docker exec -it namenode hdfs dfs -mkdir -p /user/spark/applicationHistory
-docker cp ${SCRIPT_PATH}/scripts/spark-hive-site.xml spark-master:/spark/conf/hive-site.xml
-docker cp ${SCRIPT_PATH}/scripts/spark-defaults.conf spark-master:/spark/conf/spark-defaults.conf
-docker exec -itd spark-master /spark/bin/spark-class org.apache.spark.deploy.history.HistoryServer
+docker cp "${SCRIPT_PATH}/scripts/spark-hive-site.xml" spark-master:/spark/conf/hive-site.xml
+docker cp "${SCRIPT_PATH}/scripts/spark-defaults.conf" spark-master:/spark/conf/spark-defaults.conf
+
+# Menjalankan History Server
+docker exec -d spark-master /spark/bin/spark-class org.apache.spark.deploy.history.HistoryServer
+
+echo "Selesai!"
